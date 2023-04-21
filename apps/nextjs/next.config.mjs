60415f4982225f18ae28ace7930a330be4f3613c
Clemens Pflaum <clemens.pflaum@gmail.com>
2023-04-21 19:04:55 +0200

    clerk nextjs

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -7,8 +7,9 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  // swcMinify: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
+  transpilePackages: ["@acme/api", "@acme/db"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
   typescript: { ignoreBuildErrors: !!process.env.CI },
--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+import { UserButton, useAuth } from "@clerk/nextjs";
 
 import { api, type RouterOutputs } from "~/utils/api";
 
@@ -135,27 +136,47 @@ const Home: NextPage = () => {
 export default Home;
 
 const AuthShowcase: React.FC = () => {
+  const { isSignedIn } = useAuth();
   const { data: session } = api.auth.getSession.useQuery();
+  console.log(session);
 
   const { data: secretMessage } = api.auth.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: !!session?.user },
+    { enabled: !!isSignedIn },
   );
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      {session?.user && (
+      {isSignedIn && (
+        <>
+          <p className="text-center text-2xl text-white">
+            {secretMessage && (
+              <span>
+                {" "}
+                {secretMessage} click the user button!
+                <br />
+              </span>
+            )}
+          </p>
+          <div className="flex items-center justify-center">
+            <UserButton
+              appearance={{
+                elements: {
+                  userButtonAvatarBox: {
+                    width: "3rem",
+                    height: "3rem",
+                  },
+                },
+              }}
+            />
+          </div>
+        </>
+      )}
+      {!isSignedIn && (
         <p className="text-center text-2xl text-white">
-          {session && <span>Logged in as {session?.user?.name}</span>}
-          {secretMessage && <span> - {secretMessage}</span>}
+          <Link href="/sign-in">Sign In</Link>
         </p>
       )}
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={session ? () => void signOut() : () => void signIn()}
-      >
-        {session ? "Sign out" : "Sign in"}
-      </button>
     </div>
   );
 };
