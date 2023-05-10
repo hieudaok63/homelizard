a87ad5aff65ef9fe0ae5b8d74073a4e9660db1e6
Clemens Pflaum <clemens.pflaum@gmail.com>
2023-05-10 16:51:39 +0200

    TabBar with accordion animation

--- apps/expo/src/components/navigation/TabBar.tsx
+++ apps/expo/src/components/navigation/TabBar.tsx
@@ -1,11 +1,35 @@
-import React, { useState } from "react";
-import { TouchableWithoutFeedback, View } from "react-native";
+import React, { useRef, useState, type ComponentProps } from "react";
+import {
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import Animated, {
+  Transition,
+  Transitioning,
+  useAnimatedProps,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+import { Circle, Defs, Mask, Rect, Svg } from "react-native-svg";
 import HomeIcon from "@assets/icons/HomeIcon.svg";
 import MenuIcon from "@assets/icons/MenuIcon.svg";
 import PeopleIcon from "@assets/icons/PeopleIcon.svg";
 import PersonIcon from "@assets/icons/PersonIcon.svg";
 import PlusIcon from "@assets/icons/PlusIcon.svg";
 import { type BottomTabBarProps } from "@react-navigation/bottom-tabs";
+
+const transition = (
+  <Transition.Together>
+    <Transition.In type="fade" durationMs={200} />
+    <Transition.Change />
+    <Transition.Out type="fade" durationMs={200} />
+  </Transition.Together>
+);
+
+const AnimatedRect = Animated.createAnimatedComponent(Rect);
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 const TabBar = ({
   descriptors,
@@ -14,38 +38,138 @@ const TabBar = ({
   state,
 }: BottomTabBarProps) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+
   // console.log({descriptors,insets,navigation,state});
+
+  const animatedProps = useAnimatedProps(() => {
+    return {
+      height: withTiming(open ? 200 : 64, { duration: 300 }),
+    };
+  });
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      height: withTiming(open ? 200 : 64, { duration: 300 }),
+    };
+  });
 
   return (
     <View className="flex items-center">
-      <View
-        style={{
-          height: open ? 200 : 64,
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 8,
-          },
-          shadowOpacity: 0.25,
-          shadowRadius: 3.84,
-          elevation: 5,
-        }}
-        className="bg-dark absolute bottom-6 flex w-[327px] justify-between rounded-[32px]"
+      <Transitioning.View
+        ref={ref}
+        transition={transition}
+        // className="bg-dark absolute bottom-6 flex w-[327px] justify-between rounded-[32px]"
+        className="absolute bottom-6 flex w-[328px] justify-between"
       >
-        <View className="absolute -top-7 flex w-full items-center">
-          <TouchableWithoutFeedback
-            onPress={() => {
-              setOpen(!open);
-            }}
-          >
-            <View className="bg-dark rounded-full p-4">
-              <PlusIcon />
-            </View>
-          </TouchableWithoutFeedback>
-        </View>
-        <View className="flex flex-row justify-between"></View>
+        {/* <AnimatedView
+        style={animatedStyle}
+        className="absolute bottom-6 flex w-[328px] justify-between"
+        // className="bg-dark absolute bottom-6 flex w-[327px] justify-between rounded-[32px]"
+      > */}
+        {/* <Svg
+          style={{
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 8,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }}
+          className="absolute"
+        >
+          <Defs>
+            <Mask id="mask">
+              <AnimatedRect
+                animatedProps={animatedProps}
+                width="100%"
+                fill="white"
+              />
+              <Circle r="32" cx="50%" cy="0" fill="black" />
+            </Mask>
+          </Defs>
+          <AnimatedRect
+            fill="#262636"
+            animatedProps={animatedProps}
+            width="100%"
+            rx="32"
+            mask="url(#mask)"
+          />
+        </Svg> */}
 
-        <View className="flex flex-row justify-between">
+        <Svg
+          style={{
+            height: 32,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 8,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }}
+        >
+          <Defs>
+            <Mask id="mask">
+              <Rect width="100%" height="100%" fill="white" />
+              <Circle r="32" cx="50%" cy="0" fill="black" />
+            </Mask>
+          </Defs>
+          <Rect
+            fill="#262636"
+            width="100%"
+            height="200%"
+            rx="32"
+            mask="url(#mask)"
+          />
+        </Svg>
+        <CenterButton
+          onPress={() => {
+            ref.current.animateNextTransition();
+            setOpen(!open);
+          }}
+        />
+        <View className="bg-dark">
+        {open && (
+          <View className="flex overflow-hidden pb-8">
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => navigation.navigate("Home")}
+              >
+                <View className="flex flex-row p-2">
+                  <Text className="text-white">Home</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => navigation.navigate("Profile")}
+              >
+                <View className="flex flex-row p-2">
+                  <Text className="text-white">Profile</Text>
+                </View>
+              </TouchableOpacity>
+          </View>
+        )}
+          </View>
+        <Svg
+          style={{
+            height: 32,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 8,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }}
+        >
+          <Rect fill="#262636" y="-32" width="100%" height="200%" rx="32" />
+        </Svg>
+
+        <View className="absolute bottom-0 left-0 right-0 flex flex-row justify-between">
           <View className="flex flex-row px-2 py-1">
             <View className="p-4">
               <HomeIcon />
@@ -60,7 +184,9 @@ const TabBar = ({
             </View>
             <TouchableWithoutFeedback
               onPress={() => {
+                ref.current.animateNextTransition();
                 setOpen(!open);
+                // animatedHeight.value = animatedHeight.value === 64 ? 200 : 64;
               }}
             >
               <View className="p-4">
@@ -69,9 +195,32 @@ const TabBar = ({
             </TouchableWithoutFeedback>
           </View>
         </View>
-      </View>
+      </Transitioning.View>
     </View>
   );
 };
 
 export default TabBar;
+
+const CenterButton = (props: ComponentProps<typeof TouchableOpacity>) => {
+  return (
+    <View
+          style={{
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 8,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }}
+     className="absolute -top-7 flex w-full items-center">
+      <TouchableOpacity {...props}>
+        <View className="bg-dark rounded-full p-4">
+          <PlusIcon />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
