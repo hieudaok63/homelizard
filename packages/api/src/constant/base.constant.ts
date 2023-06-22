8f61021f6e5cbed9db4f457bd065320dd1ac0595
Truong Trinh <133640733+TruongAdamoSoft@users.noreply.github.com>
2023-06-22 13:41:28 +0700

    update place of work endpoint (#20)

---
+++ packages/api/src/constant/base.constant.ts
@@ -0,0 +1,1 @@
+export const MOBILE_PHONE_REGEX = /^[+-]?\d+(\.\d+)?$/;
--- packages/api/src/router/user.ts
+++ packages/api/src/router/user.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 import { genderSchema } from "@homelizard/schema";
 
+import { MOBILE_PHONE_REGEX } from "../constant/base.constant";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
@@ -52,18 +53,99 @@ export const userRouter = createTRPCRouter({
         gender: genderSchema.optional(),
         firstName: z.string().min(1).optional(),
         lastName: z.string().min(1).optional(),
+        middleName: z.string().min(1).optional(),
+        suffix: z.string().min(1).optional(),
+        title: z.string().min(1).optional(),
+        birthday: z.date().optional(),
+        mobilePhone: z
+          .string()
+          .regex(MOBILE_PHONE_REGEX, {
+            message: "Mobile phone must be a string of numbers",
+          })
+          .optional(),
+        website: z.string().min(1).optional(),
+        address: z
+          .object({
+            street: z.string(),
+            city: z.string(),
+            zipCode: z.string(),
+            country: z.string(),
+          })
+          .optional(),
       }),
     )
     .mutation(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findUnique({
+        where: { externalId: ctx.auth.userId },
+      });
+
       return ctx.prisma.user.update({
         where: { externalId: ctx.auth.userId },
-        data: input,
+        data: {
+          ...input,
+          address: input.address
+            ? user?.addressId
+              ? { update: input.address }
+              : { create: input.address }
+            : undefined,
+        },
+      });
+    }),
+  editPlaceOfWork: protectedProcedure
+    .input(
+      z.object({
+        position: z.string(),
+        company: z.string(),
+        since: z.date(),
+        phone: z.string().regex(MOBILE_PHONE_REGEX, {
+          message: "Mobile phone must be a string of numbers",
+        }),
+        email: z.string().email(),
+        web: z.string(),
+        address: z.object({
+          street: z.string(),
+          city: z.string(),
+          zipCode: z.string(),
+          country: z.string(),
+        }),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findUnique({
+        where: { externalId: ctx.auth.userId },
+      });
+
+      if (!user?.placeOfWorkId) {
+        const newPlaceOfWork = await ctx.prisma.user.update({
+          where: { id: user?.id },
+          data: {
+            placeOfWork: {
+              create: {
+                ...input,
+                address: input.address ? { create: input.address } : undefined,
+              },
+            },
+          },
+        });
+
+        return newPlaceOfWork;
+      }
+      return ctx.prisma.placeOfWork.update({
+        where: { id: user?.placeOfWorkId },
+        data: {
+          ...input,
+          address: { update: input.address },
+        },
       });
     }),
 
   userInfo: protectedProcedure.query(async ({ ctx }) => {
     const user = await ctx.prisma.user.findUnique({
       where: { externalId: ctx.auth.userId },
+      include: {
+        placeOfWork: true,
+        address: true,
+      },
     });
 
     if (!user) {
