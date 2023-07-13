b3f7faa93e4438f5f93aa929769b6dd997829d29
TruongAdamoSoft <truong.trinh@adamo.software>
2023-07-13 08:44:14 +0700

    create seed data for initial database

--- packages/db/prisma/data.ts
+++ packages/db/prisma/data.ts
@@ -70,3 +70,109 @@ export const objectStyles = [
     imageUrl: "assets/objectStyleImage/Transitional.png",
   },
 ];
+
+export const usersData = [
+  {
+    externalId: "user_2S98SP1dInHBqr22ZJpypvfWJsP",
+    email: "trinhtruong1803@gmail.com",
+    firstName: "Veronica",
+    lastName: "Schneider",
+    gender: "female",
+  },
+];
+
+export const searchProfilesData = [
+  {
+    objectType: "House",
+    livingAreaSize: 300,
+    latitude: 10,
+    longitude: 70,
+    radius: 24,
+    plotSize: 800,
+    roomAmount: 3,
+    startYearOfConstruction: 2003,
+    endYearOfConstruction: 2015,
+    availability: new Date("03/12/2015"),
+    purchaseType: "buy",
+    minPrice: 1000,
+    maxPrice: 2000,
+  },
+  {
+    objectType: "House",
+    livingAreaSize: 100,
+    latitude: 20,
+    longitude: 50,
+    radius: 30,
+    plotSize: 400,
+    roomAmount: 2,
+    startYearOfConstruction: 2010,
+    endYearOfConstruction: 2023,
+    availability: new Date("03/15/2012"),
+    purchaseType: "buy",
+    minPrice: 1000,
+    maxPrice: 2000,
+  },
+  {
+    objectType: "Apartment",
+    livingAreaSize: 100,
+    latitude: 30,
+    longitude: 30,
+    radius: 12,
+    plotSize: 200,
+    roomAmount: 3,
+    startYearOfConstruction: 2011,
+    endYearOfConstruction: 2023,
+    availability: new Date("06/15/2022"),
+    purchaseType: "rent",
+    minPrice: 1000,
+    maxPrice: 2000,
+  },
+];
+
+export const realEstateObjectsData = [
+  {
+    title:
+      "Architektonisches Highlight - tolles Einfamilienhaus mit großem Garten in Fellheim!",
+    objectType: "House",
+    numberOfFloor: 3,
+    livingAreaSize: 300,
+    plotSize: 800,
+    numberOfBedroom: 3,
+    numberOfBathroom: 3,
+    roomAmount: 4,
+    description:
+      "Architektonisches Highlight - tolles Einfamilienhaus mit großem Garten in Fellheim!",
+    price: 1000,
+    imageUrl: "https://fakeimg.pl/350x200/ff0000,128/000,255",
+  },
+  {
+    title:
+      "Architektonisches Highlight - tolles Einfamilienhaus mit großem Garten in Fellheim!",
+    objectType: "House",
+    numberOfFloor: 1,
+    livingAreaSize: 200,
+    plotSize: 300,
+    numberOfBedroom: 5,
+    numberOfBathroom: 2,
+    roomAmount: 6,
+    description:
+      "Architektonisches Highlight - tolles Einfamilienhaus mit großem Garten in Fellheim!",
+    price: 1200,
+    imageUrl: "https://fakeimg.pl/350x200/ff0000,128/000,255",
+  },
+  {
+    title:
+      "Architektonisches Highlight - tolles Einfamilienhaus mit großem Garten in Fellheim!",
+    objectType: "Apartment",
+    numberOfFloor: 3,
+    livingAreaSize: 100,
+    plotSize: 200,
+    numberOfBedroom: 2,
+    numberOfBathroom: 1,
+    roomAmount: 3,
+    description:
+      "Architektonisches Highlight - tolles Einfamilienhaus mit großem Garten in Fellheim!",
+    price: 500,
+    imageUrl: "https://fakeimg.pl/350x200/ff0000,128/000,255",
+  },
+];
--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,14 +1,49 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, SearchProfile, User } from "@prisma/client";
 
-import { objectStyles } from "./data";
+import {
+  objectStyles,
+  realEstateObjectsData,
+  searchProfilesData,
+  usersData,
+} from "./data";
 
 const prisma = new PrismaClient();
 async function main() {
-  await Promise.all(
-    objectStyles.map(async (objectStyle) => {
-      return await prisma.objectStyle.create({ data: objectStyle });
-    }),
-  );
+  const user = await prisma.user.create({
+    data: usersData[0] as User,
+  });
+  const customer = await prisma.customer.create({
+    data: {
+      users: { connect: { id: user.id } },
+    },
+  });
+  for (let i = 0; i < 3; i++) {
+    const searchProfileCreate = searchProfilesData[i] as SearchProfile;
+    const searchProfile = await prisma.searchProfile.create({
+      data: {
+        ...searchProfileCreate,
+        customer: { connect: { id: customer.id } },
+        objectStyles: {
+          create: objectStyles.slice(i, i + 2).map((objectStyle) => ({
+            title: objectStyle.title,
+          })),
+        },
+      } as SearchProfile,
+    });
+    const searchResult = await prisma.searchResult.create({
+      data: {
+        searchProfile: { connect: { id: searchProfile.id } },
+        realEstate: { create: realEstateObjectsData[i] },
+      },
+    });
+
+    await prisma.favorite.create({
+      data: {
+        searchResult: { connect: { id: searchResult.id } },
+        user: { connect: { id: user.id } },
+      },
+    });
+  }
 }
 main()
   .then(async () => {
