de78a00d13cd4da0766716fbef29a0ebd3bb5794
Clemens Pflaum <clemens.pflaum@gmail.com>
2023-07-21 14:24:58 +0200

    replace blue corner image with css triangle

--- apps/expo/src/components/ui/ButtonAddFavorite.tsx
+++ apps/expo/src/components/ui/ButtonAddFavorite.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Image,
-  TouchableOpacity,
-  View,
-  type ImageSourcePropType,
-} from "react-native";
+import { TouchableOpacity, View } from "react-native";
 
 import CheckIcon from "@assets/icons/CheckIcon.svg";
 import PlusIcon from "@assets/icons/PlusIcon.svg";
@@ -20,16 +15,18 @@ export const ButtonAddFavorite = ({
 }: ButtonAddFavorite) => {
   return (
     <View className="w-20">
-      {/* TODO: replace image with a corner made in css */}
-      <Image
-        source={require<ImageSourcePropType>("../../../assets/objectStyleImage/Subtract.png")}
-      />
+      <TriangleCorner />
       <TouchableOpacity
-        className="absolute bottom-3 right-2"
+        className="absolute bottom-4 right-4"
         onPress={onPressSelect}
       >
         {selected ? <CheckIcon /> : <PlusIcon />}
       </TouchableOpacity>
     </View>
+  );
+};
+const TriangleCorner = () => {
+  return (
+    <View className="border-b-[100px] border-l-[100px] border-b-blue_1 border-l-transparent" />
   );
 };
