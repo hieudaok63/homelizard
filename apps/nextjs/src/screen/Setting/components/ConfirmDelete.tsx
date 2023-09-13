a36c45da6f8b0531d7a35085be47f768f4d90328
hieudaotrung2027 <137464209+hieudaotrung2027@users.noreply.github.com>
2023-09-13 21:30:42 +0700

    delete account (#99)

--- apps/nextjs/src/screen/Setting/components/ConfirmDelete.tsx
+++ apps/nextjs/src/screen/Setting/components/ConfirmDelete.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useAuth } from "@clerk/nextjs";
 
+import { api } from "~/utils/api";
+import { showAppToast } from "~/utils/toast";
 import { warning } from "~/assets";
 import { Button } from "~/components";
+import { PATH_SIGN_IN } from "~/constants/navigation";
 import { useToggleStore } from "~/zustand/store";
 
 export default function ConfirmDelete() {
   const setToggleBtn = useToggleStore((state) => state.setToggleButton);
+  const { data } = api.user.userInfo.useQuery();
+  const { signOut } = useAuth();
+  const router = useRouter();
+
+  const deleteAccountMutation = api.user.deleteAccount.useMutation({
+    async onSuccess() {
+      await signOut();
+      router.push(PATH_SIGN_IN);
+      showAppToast(`Delete account success`, "success");
+    },
+    onError(error) {
+      console.log(error);
+    },
+  });
+
+  const handleDeleteAccount = async () => {
+    await deleteAccountMutation.mutateAsync();
+  };
+
   return (
     <>
       <div className="flex w-[550px] flex-col items-center bg-white p-6 shadow-4xl">
@@ -21,7 +45,7 @@ export default function ConfirmDelete() {
         </span>
 
         <p className="mb-2 text-lg text-grey">E-Mailadresse</p>
-        <input type="text" className="w-full border-[1px] p-2" />
+        <div className="text-xl font-semibold text-red-600">{data?.email}</div>
         <div className="absolute bottom-8 flex rounded-3xl">
           <Button
             className="mr-5 w-[100px] rounded-3xl"
@@ -30,7 +54,12 @@ export default function ConfirmDelete() {
             Back
           </Button>
 
-          <Button className="w-[200px] rounded-3xl">Bestätigen</Button>
+          <Button
+            className="w-[200px] rounded-3xl"
+            onClick={handleDeleteAccount}
+          >
+            Bestätigen
+          </Button>
         </div>
       </div>
     </>
